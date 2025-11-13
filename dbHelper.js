748d3996e408c99e90ab82ae30414ae04ca5c088
Nabhil Irtisham <nabhil.irtisham@gmail.com>
2025-11-12 20:04:12 -0500

    Edit dbHelper.js

---
+++ dbHelper.js
@@ -0,0 +1,2 @@
+const mysql = require('mysql2/promise');                // promise API to interact w/ MySQL DBs
+require('dotenv').config()                              // import and configure .env file
