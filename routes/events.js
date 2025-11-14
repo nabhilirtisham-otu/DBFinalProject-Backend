aa1a6a0e41888d8b3663c7990859f79ab703e6d6
Nabhil Irtisham <nabhil.irtisham@gmail.com>
2025-11-13 23:45:24 -0500

    Edit events.js

--- routes/events.js
+++ routes/events.js
@@ -2,6 +2,7 @@ const expressLib = require('express');                                  //Import
 const dbHelper = require('../dbHelper.js');                             //Import DB connection
 const { validAuth, validRole } = require('../middleware/auth.js');      //Import middleware authentication functions
 const { validNum, validString, validDate} = require('../utils/validatorFunctions.js'); //Import validator functions from utils
+const { param } = require('./auth.js');
 
 const expRouter = expressLib.Router();                                  //Router instance
 
@@ -9,12 +10,43 @@ const expRouter = expressLib.Router();                                  //Router
 expRouter.get('/', async(req, res) => {
     try{
         const { q, category, city, startDate, endDate, limit, offset=0 } = req.query;   //Extra URL query string parameters
-        const conditions = [];                                          //Stores WHERE clause conditions  
-        const params = [];                                              //Stores actual parameter values
+        const queryConditions = [];                                     //Stores WHERE clause conditions  
+        const queryParams = [];                                         //Stores actual parameter values
 
         if (q){
-            conditions.push('(e.title LIKE ? OR e.event_description LIKE ?)');      //Add a condition matching the event title/description
-            params.push(`%${q}%`, `%${q}%`);                                        //Partial matching wildcard pattern, added to parameters
+            queryConditions.push('(e.title LIKE ? OR e.event_description LIKE ?)');     //Add a condition matching the event title/description
+            queryParams.push(`%${q}%`, `%${q}%`);                                       //Partial matching wildcard pattern, added to parameters
+        }
+        if (city){
+            queryConditions.push('v.city = ?');                         //Filter events by venue city
+            queryParams.push(city);
+        }
+        if (startDate){
+            queryConditions.push('e.start_time >= ?');                  //Filter events by start-end date range
+            queryParams.push(startDate);
+        }
+        if (endDate){
+            queryConditions.push('e.start_time <= ?');
+            queryParams.push(endDate);
+        }
+
+        //Retrieves event details and venue info, allowing filtering by category
+        let baseQuery = `
+            SELECT DISTINCT e.event_id, e.title, e.start_time, e.end_time, e.standard_price,
+                e.event_status, v.venue_name, v.city
+            FROM Event_ e
+            JOIN Venue v ON e.venue_id = v.venue_id
+            LEFT JOIN Event_Category ec ON e.event_id = ec.event_id
+            LEFT JOIN Category c ON ec.category_id = c.category_id
+        `;
+
+        if (category){                                                  //Filter events by category ID
+            queryConditions.push('c.category_id = ?');
+            queryParams.push(category);
+        }
+
+        if (queryConditions.length){
+            baseQuery += ' WHERE ' + queryConditions.join( ' AND ');    //Concatenate filters in queryConditions into baseQuery's WHERE clause
         }
     }
 })
