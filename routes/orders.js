9d722f397184c2c149c16bbb567a91002437e4ca
Nabhil Irtisham <nabhil.irtisham@gmail.com>
2025-11-18 13:29:10 -0500

    /

--- routes/orders.js
+++ routes/orders.js
@@ -11,7 +11,7 @@ const expRouter = expressLib.Router();
 
 // POST endpoint to create order + payment, and mark tickets as sold in a transaction
 expRouter.post("/", validAuth, async (req, res) => {
-    const uID = req.session.user.id;
+    const uID = req.session.user.users_id || req.session.user.id;
     if (!uID) {
         return res.status(401).json({ error: "Not authenticated." });
     }
